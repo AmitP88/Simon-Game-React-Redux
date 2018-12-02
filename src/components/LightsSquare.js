6eee6b07e0a017182ef9e27c2144574550c54edb
amitp <amitpatel_7@msn.com>
2018-12-02 09:50:16 -0800

    created new component LightSquare

---
+++ src/components/LightsSquare.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class LightsSquare extends Component {
+    render() {
+        return (
+            <div className="LightsSquare-container"></div>
+        );
+    }
+}
+
+export default LightsSquare;
