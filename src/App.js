2879c7c266b0821a9383a1932a050b7b20d4c9cc
amitp <amitpatel_7@msn.com>
2018-12-03 11:39:37 -0800

    made gamepad responsive for the most part - need to align Controls component horizontally relative to LightsSquare

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './media_queries.css';
 
 import LightsSquare from './components/LightsSquare';
 import Controls from './components/Controls';
