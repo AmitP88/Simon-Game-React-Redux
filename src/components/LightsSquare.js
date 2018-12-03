626c3bdc0caff292e56dde1b369d908122771bb5
amitp <amitpatel_7@msn.com>
2018-12-02 23:37:06 -0800

    added triangles as simon game buttons for pad

--- src/components/LightsSquare.js
+++ src/components/LightsSquare.js
@@ -3,7 +3,16 @@ import React, { Component } from 'react';
 class LightsSquare extends Component {
     render() {
         return (
-            <div className="LightsSquare-container"></div>
+            <div className="LightsSquare-container">
+                <div className="topHalf">
+                    <div className="Square red"></div>
+                    <div className="Square green"></div>                
+                </div>
+                <div className="bottomHalf">
+                    <div className="Square blue"></div>
+                    <div className="Square purple"></div>
+                </div>
+            </div>
         );
     }
 }
