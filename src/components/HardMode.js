4f87009a94ba3db551968917a2e74b38634d02e2
amitp <amitpatel_7@msn.com>
2018-12-03 08:24:09 -0800

    added hardmode toggle switch to HardMode component

--- src/components/HardMode.js
+++ src/components/HardMode.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react';
 class HardMode extends Component {
     render() {
         return(
-            <div className="HardMode-container"></div>
+            <div className="HardMode-container">
+                <label className="easy">Easy</label>
+                <label className="switch hardmode">
+                    <input type="checkbox" />
+                    <span className="slider round"></span>
+                </label>
+                <label className="hard">Hard</label>
+            </div>
         );
     }
 }
