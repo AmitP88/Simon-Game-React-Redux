657b30844c8413aad1844e795935a71118989993
amitp <amitpatel_7@msn.com>
2018-12-03 06:49:16 -0800

    completed styles for counter component

--- src/components/LevelCounter.js
+++ src/components/LevelCounter.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class LevelCounter extends Component {
     render() {
         return (
-            <div className="LevelCounter-container"></div>
+            <div className="LevelCounter-container">
+                <div className="LevelCounter"></div>
+                <div className="LevelCounter-label">Level</div>
+            </div>
         );
     }
 }
