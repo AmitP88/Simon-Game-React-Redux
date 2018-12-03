db16a2ccf4a84e8f23cc73d5a2697bdc9bd18d8a
amitp <amitpatel_7@msn.com>
2018-12-03 04:15:12 -0800

    created new component StartButton and added styles to it

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import OnOffSwitch from './OnOffSwitch'; 
+import OnOffSwitch from './OnOffSwitch';
+import StartButton from './StartButton';
 
 class Controls extends Component {
     render() {
         return (
             <div className="Controls">
                 <OnOffSwitch />
+                <StartButton />
             </div>
         );
     }
---
+++ src/components/StartButton.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class StartButton extends Component {
+    render() {
+        return (
+            <div className="StartButton-container"></div>
+        );
+    }
+}
+
+export default StartButton;
