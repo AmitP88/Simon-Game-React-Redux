f71e06c2961137fac85efb5d629a11b2540055d2
amitp <amitpatel_7@msn.com>
2018-12-04 02:03:16 -0800

    deleted media queries css file and centered Controls component on top of LightsSquare component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './media_queries.css';
-
 import LightsSquare from './components/LightsSquare';
 import Controls from './components/Controls';
 
