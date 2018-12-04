1242271c09d65971fdc2986efe3380d3c20c76fd
amitp <amitpatel_7@msn.com>
2018-12-04 10:47:14 -0800

    light up triangles onclick

--- src/components/LightsSquare.js
+++ src/components/LightsSquare.js
@@ -1,16 +1,57 @@
 import React, { Component } from 'react';
 
 class LightsSquare extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            red: 'dark_red',
+            green: 'dark_green',
+            blue: 'dark_blue',
+            purple: 'dark_purple'
+        }
+        this.handleOnClick = this.handleOnClick.bind(this);
+    }
+
+    handleOnClick(e) {
+        e.preventDefault();
+        
+        switch(e.target.id){
+            case 'red':
+                this.setState({
+                    red: 'light_red'
+                }, () => console.log(this.state.red));
+                break;
+            case 'green':
+                this.setState({
+                    green: 'light_green'
+                }, () => console.log(this.state.green));
+                break;
+            case 'blue':
+                this.setState({
+                    blue: 'light_blue'
+                }, () => console.log(this.state.blue));
+                break;
+            case 'purple':
+                this.setState({
+                    purple: 'light_purple'
+                }, () => console.log(this.state.purple));
+                break;
+            default:
+                console.log(e.target.id);
+                break;
+        }
+    }
+
     render() {
         return (
             <div className="LightsSquare-container">
                 <div className="topHalf">
-                    <div className="dark_red"></div>
-                    <div className="dark_green"></div>                
+                    <div id="red" className={this.state.red} onClick={this.handleOnClick}></div>
+                    <div id="green" className={this.state.green} onClick={this.handleOnClick}></div>                
                 </div>
                 <div className="bottomHalf">
-                    <div className="dark_blue"></div>
-                    <div className="dark_purple"></div>
+                    <div id="blue" className={this.state.blue} onClick={this.handleOnClick}></div>
+                    <div id="purple" className={this.state.purple} onClick={this.handleOnClick}></div>
                 </div>
             </div>
         );
