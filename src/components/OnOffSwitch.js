89dd838c9c291b94fc73f01be159f07a6cc44b11
amitp <amitpatel_7@msn.com>
2018-12-04 13:05:45 -0800

    created state and handleChange method for OnOffSwitch component

--- src/components/OnOffSwitch.js
+++ src/components/OnOffSwitch.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 
 class OnOffSwitch extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            on: false
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange() {
+        if(this.state.on === false) {
+            this.setState({ 
+             on: true 
+            }, () => console.log(this.state.on));
+        } else if (this.state.on === true) {
+            this.setState({
+             on: false
+            }, () => console.log(this.state.on));
+        }
+        
+    }
+
     render() {
         return (
             <div className="OnOffSwitch-container">
                 <label className="Off-label">Off</label>
                 <label className="switch">
-                    <input type="checkbox" />
+                    <input type="checkbox" onChange={this.handleChange} />
                     <span className="slider round"></span>
                 </label>
                 <label className="On-label">On</label>
