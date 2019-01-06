a0a621af5e55cdafabbea5ffdaff8954743bf626
amitp <amitpatel_7@msn.com>
2019-01-05 16:24:56 -0800

    Updates

---
+++ precache-manifest.e58d85e1c50af9f4cf4b07aa75a9f81b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4a912ee52e2c884a80499208c5568cc",
+    "url": "/Simon-Game-React-Redux/static/media/simonSound4.f4a912ee.mp3"
+  },
+  {
+    "revision": "281006edea5a358ff37d",
+    "url": "/Simon-Game-React-Redux/static/css/main.5720f4b6.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Simon-Game-React-Redux/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b4220a18ec84accff3ab",
+    "url": "/Simon-Game-React-Redux/static/js/1.b4220a18.chunk.js"
+  },
+  {
+    "revision": "55e4650c1b1d4361f309",
+    "url": "/Simon-Game-React-Redux/static/js/runtime~main.55e4650c.js"
+  },
+  {
+    "revision": "319c77102049da3622a954acd189f1eb",
+    "url": "/Simon-Game-React-Redux/static/media/simonSound1.319c7710.mp3"
+  },
+  {
+    "revision": "c3c86e71f309dde162a2e9b52cbc702f",
+    "url": "/Simon-Game-React-Redux/static/media/simonSound2.c3c86e71.mp3"
+  },
+  {
+    "revision": "f3c18b98bab33c0fbedde9cb67bdfcf2",
+    "url": "/Simon-Game-React-Redux/static/media/simonSound3.f3c18b98.mp3"
+  },
+  {
+    "revision": "281006edea5a358ff37d",
+    "url": "/Simon-Game-React-Redux/static/js/main.281006ed.chunk.js"
+  },
+  {
+    "revision": "319affeb3e9915a4de6785cdb0c8d355",
+    "url": "/Simon-Game-React-Redux/static/media/redux.319affeb.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Simon-Game-React-Redux/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Simon-Game-React-Redux/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Simon-Game-React-Redux/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Simon-Game-React-Redux/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "b4220a18ec84accff3ab",
+    "url": "/Simon-Game-React-Redux/static/css/1.7aa6a71f.chunk.css"
+  },
+  {
+    "revision": "b2b83ab298deab54afa602d4de1dfcb2",
+    "url": "/Simon-Game-React-Redux/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Simon-Game-React-Redux/precache-manifest.e58d85e1c50af9f4cf4b07aa75a9f81b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Simon-Game-React-Redux/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
